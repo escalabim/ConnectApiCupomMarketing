00ef0f13a061d27f3643cf4c9a7094d1d69c3fa3
João Pedro <tecnofaisca@gmail.com>
2017-05-28 22:57:43 -0300

    Primeiro commit

---
+++ ConnectApiCupomMarketing/Models/RequestResumoCupom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConnectApiCupomMarketing.Models
+{
+    public class RequestResumoCupom
+    {
+        public string api_key { get; set; }
+        public string code_coupon { get; set; }
+    }
+}
